ba08d4fdd3909e4c185c88b70dcc9c14a6059cce
codypharm <williamikeji@gmail.com>
2025-12-03 15:02:32 +0100

    vault creation done

--- apps/frontend/src/wagmi.ts
+++ apps/frontend/src/wagmi.ts
@@ -9,13 +9,17 @@ export const storyTestnet = defineChain({
     default: {
       http: [
         import.meta.env.VITE_RPC_URL ||
-          "https://rpc-storyevm-testnet.aldebaranode.xyz",
+          "https://rpc.ankr.com/story_aeneid_testnet/bc16a42ff54082470945f1420d9917706e7de9dbea9c11f20d93584bd6d26886",
+        "https://rpc-storyevm-testnet.aldebaranode.xyz",
+        "https://testnet.storyrpc.io",
       ],
     },
     public: {
       http: [
         import.meta.env.VITE_STORY_RPC ||
-          "https://rpc-storyevm-testnet.aldebaranode.xyz",
+          "https://rpc.ankr.com/story_aeneid_testnet/bc16a42ff54082470945f1420d9917706e7de9dbea9c11f20d93584bd6d26886",
+        "https://rpc-storyevm-testnet.aldebaranode.xyz",
+        "https://testnet.storyrpc.io",
       ],
     },
   },
@@ -27,7 +31,12 @@ export const config = createConfig({
   transports: {
     [storyTestnet.id]: http(
       import.meta.env.VITE_RPC_URL ||
-        "https://rpc-storyevm-testnet.aldebaranode.xyz"
+        "https://rpc.ankr.com/story_aeneid_testnet/bc16a42ff54082470945f1420d9917706e7de9dbea9c11f20d93584bd6d26886",
+      {
+        timeout: 10_000,
+        retryCount: 3,
+        retryDelay: 1000,
+      }
     ),
   },
   //   autoConnect: true,
